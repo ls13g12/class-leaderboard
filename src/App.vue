3b8160c90a732a76cd0ff3e080d38f3a08ad6925
ls13g12 <luke.stoneman.94@gmail.com>
2022-05-18 22:25:06 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AddPlayers from "./components/AddPlayers.vue";
+export default {
+  components: { AddPlayers },
+  data() {
+    return {
+      players: [],
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <AddPlayers v-model:players="players" />
+    <ul>
+      <li v-for="player in players" :key="player.name">
+        {{ player.name }} with a score of {{ player.score }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/AddPlayers.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: ["players"],
+  emits: ["update:players"],
+  data() {
+    return {
+      players_entered: "",
+      temp_players: [],
+    };
+  },
+  methods: {
+    //user submits a names into the sidebar, one per line, and they are stored as a sorted list
+    submit_names() {
+      this.temp_players = [];
+      let _players = this.players_entered
+        .split("\n")
+        .filter((n) => n)
+        .sort();
+      for (let player of _players) {
+        let new_player = { name: player, score: 0 };
+        this.temp_players.push(new_player);
+      }
+      //sort all players by name
+      this.temp_players.sort((a, b) => (a.name > b.name ? 1 : -1));
+      this.$emit("update:players", this.temp_players);
+    },
+  },
+};
+</script>
+
+<template>
+  <textarea name="players" v-model="players_entered"></textarea>
+  <button type="button" @click="submit_names()">Submit</button>
+</template>
